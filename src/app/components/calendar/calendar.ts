ceb0d02185fda096a8fd024c3606db32dba604d1
Tomasz Kopytek <tomek.kopytek@gmail.com>
2026-05-21 19:00:46 +0200

    Create base layout and standalone components

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
+  standalone:true,
   imports: [],
   templateUrl: './calendar.html',
   styleUrl: './calendar.css',
